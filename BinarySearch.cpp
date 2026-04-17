4284f6c861b8f5967698d32f41693b0910a36006
fairus0305 <ruszezzy@gmail.com>
2026-04-17 15:33:28 +0700

    Menambahkan logika pencarian menggunakan metode binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -76,10 +76,32 @@ void binarySearch()
         cout << "====================================\n";
 
         cout << "Masukan elemen yang ingin dicari : ";
-        cin >> k;
-        
-    } while ({
-        
-    });
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do 
+        {
+            int mid = (low + high) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[✔] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+        }
+
+        if (x > element[mid])
+        {
+            high - mid - 1;
+        }
+
+        if (x > element[mid])
+        {
+            low = mid + 1;
+        }
+
+    } 
     
 }
